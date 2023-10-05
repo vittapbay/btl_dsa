43ce474c0d7875a5f46bd62535fdf9e2dc13b07b
vittapbay <131147129+vittapbay@users.noreply.github.com>
2023-10-05 12:34:09 +0700

    btl1_test.cpp

--- btl1_test.cpp
+++ btl1_test.cpp
@@ -6,11 +6,54 @@ class imp_res : public Restaurant
 		imp_res() {};
 		customer*X;
 		int totalcustomer=0;
-		customer*wait;
+		customer*waitFront;
+		customer*waitBack;
 		int line=0;
+		bool sameName(string name,customer* cus){
+			
+		}
 		void RED(string name, int energy)
-		{	
-			if(totalcustomer<MAXSIZE)
+		{	if(!energy) return;
+			if(totalcustomer<MAXSIZE/2){
+				if(!totalcustomer){
+					customer *cus = new customer (name, energy, nullptr, nullptr);//name eneegy prev next
+					X=cus;
+					totalcustomer++;
+				}
+				else{
+					if(energy<X->energy){
+						customer *cus = new customer (name, energy, X->prev, X);
+						X=cus;
+						totalcustomer++;
+					}
+					else{
+						customer *cus = new customer (name, energy, X, X->next);
+						X=cus;
+						totalcustomer++;
+					}
+				}
+			}
+			else{
+				if(totalcustomer<MAXSIZE){
+
+					totalcustomer++;
+				}
+				else{
+					if(line<MAXSIZE){
+						if(!line){
+							customer *cus = new customer (name, energy, nullptr, nullptr);
+							totalcustomer++;
+							waitBack=waitFront=cus;
+						}
+						else{
+							customer *cus = new customer (name, energy, waitBack, nullptr);
+							waitBack->next=cus;
+							waitBack=cus;
+							totalcustomer++;
+						}
+					}
+				}
+			}
 			cout << name << " " << energy << endl;
 			customer *cus = new customer (name, energy, nullptr, nullptr);
 		}
