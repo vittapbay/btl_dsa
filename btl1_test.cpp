1905caea0cec60e8bac4d1e174068f4835ce57ea
vittapbay <131147129+vittapbay@users.noreply.github.com>
2023-10-25 15:03:56 +0700

    Add files via upload

--- btl1_test.cpp
+++ btl1_test.cpp
@@ -3,10 +3,7 @@
 class imp_res : public Restaurant
 {
 	public:	
-		class wating
-		{		
-			public:
-			class Node{
+					class Node{
 				public:
 				customer*cust;
 				int index;
@@ -28,7 +25,8 @@ class imp_res : public Restaurant
 					cust=nullptr;
 				}
 			};
-			
+		class wating
+		{		
 		public:
 			int line=0;
 			Node*head;
@@ -47,6 +45,16 @@ class imp_res : public Restaurant
 					line--;				
 				}
 				head=tail=nullptr;
+			}
+			void clearTable(int& totalcustomer){
+				while(totalcustomer){
+					Node*temp=head->next;
+					deleteCust(head->cust,nullptr);
+					delete head;
+					head=temp;	
+					line--;	
+					totalcustomer--;			
+				}
 			}
 			void deleteCust(customer*cust,customer*x){
 				if(cust->prev!=nullptr&&cust->next!=nullptr){
@@ -91,17 +99,18 @@ class imp_res : public Restaurant
 				}
 				
 			}
-			void current (int index){//dung cho shellsort
+			void current (){//dung cho shellsort
 					Node*temp=head;
-					for(int i=0;i<index;i++){
+					for(int i=0;i<line;i++){
+						temp->cust->print();
 						temp=temp->next;
 					}
-					cout<<temp->getName()<<" "<<temp->getEnergy();
+					
 					return ;
 				}
-			void deleteNode(int num, customer *x){//khi xoa truyen vi tri xoa
+			void deleteNode(int num, customer *x,int& total){//khi xoa truyen vi tri xoa
 				if(num>=line||num>MAXSIZE){
-					this->clear();				
+					this->clearTable(total);				
 				}
 				else if(num==1){
 					if(line>2){
@@ -116,6 +125,7 @@ class imp_res : public Restaurant
 						head=tail;
 					}
 					line--;
+					total--;
 				}
 				else{
 					Node*temp=head;
@@ -129,6 +139,7 @@ class imp_res : public Restaurant
 					delete temp;
 					head->prev=nullptr;
 					line--;
+					total--;
 				}
 			}
 			bool chekName(string name){
@@ -179,7 +190,7 @@ class imp_res : public Restaurant
 		void RED(string name, int energy)
 		{	
 			if(energy==0) return;
-			if(totalcustomer<MAXSIZE/2){//so khach trong ban <max/2
+			if(totalcustomer<MAXSIZE/2+MAXSIZE%2){//so khach trong ban <max/2
 				if(totalcustomer==0){//truong hop ko co ai trong ban
 					customer *cus = new customer (name, energy, nullptr, nullptr);//name eneegy prev next
 					X=cus;
@@ -292,27 +303,125 @@ class imp_res : public Restaurant
 			
 			//cout<<0<<endl;
 		}	
+		void pluss(string name,int energy){
+			if(energy==0) return;
+			if(totalcustomer<MAXSIZE/2+MAXSIZE%2){//so khach trong ban <max/2
+				if(totalcustomer==0){//truong hop ko co ai trong ban
+					customer *cus = new customer (name, energy, nullptr, nullptr);//name eneegy prev next
+					X=cus;
+					totalcustomer++;
+				}
+				else{
+					if(sameName(name))return;//check same name in table
+					if(energy<X->energy){//energy<X   --->them nguoc chieu kim dong ho
+					
+						//ng 2 dc vao
+							customer *cus = new customer (name, energy, X->prev, X);//can fix
+							if(X->prev==nullptr){
+								X->next=cus;
+								cus->prev=X;        //1 pt---
+							}
+							else{
+								X->prev->next=cus;
+							}
+							X->prev=cus;
+							X=cus;
+							totalcustomer++;
+					}
+					else{// energy>= X   them cung chieu kim clock
+						
+						
+							customer *cus = new customer (name, energy, X, X->next);// can fix
+							if(cus->next!=nullptr){
+								X->next->prev=cus;
+							}
+							else{//so pt htai la 1
+								X->prev=cus;
+								cus->next=X;
+							}
+							X->next=cus;
+							X=cus;
+							totalcustomer++;						
+					}
+				}
+				
+			}
+			else{// TH >max/2
+				if(sameName(name))return;//check same name in table
+				
+				if(totalcustomer<MAXSIZE){
+					int sub=abs(energy)-abs(X->energy);
+					int index=0;
+					customer*temp=X;
+					for(int i=0;i<totalcustomer;i++){
+						if(abs(abs(energy)-abs(temp->energy))>abs(sub)){
+							sub=abs(energy)-abs(temp->energy);
+							index=i;
+						}
+						if(totalcustomer>2)temp=temp->next;
+						else if(totalcustomer==2){
+							 if(temp->next)temp=temp->next;
+							 else temp=temp->prev;
+						}
+					}
+					temp=X;
+					for(int i=0;i<index;i++){
+						if(temp->next)temp=temp->next;
+						else temp=temp->prev;
+					}
+					if(sub<0){//am them nguoc chieu
+							customer *cus = new customer (name, energy, temp->prev, temp);//can fix
+							if(temp->prev==nullptr){
+								temp->next=cus;
+								cus->prev=temp;        //1 pt---
+							}
+							else{
+								temp->prev->next=cus;
+							}
+							temp->prev=cus;
+							X=cus;
+							totalcustomer++;
+					}
+					else{// khac 0 them cung chieu
+						customer *cus = new customer (name, energy, temp, temp->next);// can fix
+							if(cus->next!=nullptr){
+								temp->next->prev=cus;
+							}
+							else{//so pt htai la 1
+								temp->prev=cus;
+								cus->next=temp;
+							}
+							temp->next=cus;
+							X=cus;
+							totalcustomer++;
+					}
+				}
+				else{
+					return;
+				}
+			}
+		}
+		Node* findCust(customer* s,wating* temp){
+			Node*run=temp->head;		
+			while(run->cust!=s){
+				run=run->next;
+			}
+			return run;
+		}	
 		void BLUE(int num)
 		{	if(num<waiting4luv->getLine())
 			{
 				while (num)
 				{
-					waiting4luv->deleteNode(1,X);
+					waiting4luv->deleteNode(1,X,totalcustomer);
 					num--;
 				}
 			}
 			else{
-					waiting4luv->deleteNode(num,X);
+					waiting4luv->deleteNode(num,X,totalcustomer);
+					X=nullptr;
 			}
-			wating*tes=waiting4luv;
-			cout<<tes->getLine();
-			customer*s=X;
-			
-			for(int i=0;i<1;i++){
-				//s->print();
-				//s=s->prev;
-				//tes->current(i);cout<<endl;
-			}			
+		
 			
 			//cout << "blue "<< num << endl;
 		}
@@ -333,7 +442,24 @@ class imp_res : public Restaurant
 			//cout << "domain_expansion" << endl;
 		}
 		void LIGHT(int num)
-		{
+		{	
+				customer*s=X;
+				if(num>0){
+					for(int i=0;i<totalcustomer;i++){
+						s->print();
+						s=s->next;
+					}	
+				}
+				else if(num<0){
+					for(int i=0;i<totalcustomer;i++){
+						s->print();
+						s=s->prev;
+					}	
+				}
+			else{
+				wating*te=cust;
+				te->current();
+			}
 			//cout << "light " << num << endl;
 		}
 };
